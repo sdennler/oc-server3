750cd616b25acfce144dc73f914ef317c66e3b69
sdennler <den@vollbio.de>
2021-03-06 18:14:05 +0100

    docker:start-clean fix

--- dev-ops/docker/actions/start-clean.sh
+++ dev-ops/docker/actions/start-clean.sh
@@ -2,10 +2,10 @@
 
 docker-compose rm --stop --force
 
-git add --force .idea
+if [ -d .idea ]; then git add --force .idea; fi
 git clean -dfX
 git reset HEAD .idea
 
 docker-compose build --pull
 docker-compose up --build -d
-docker exec -t opencaching-webserver ./psh.phar docker:init
+docker exec -u __USERKEY__ -t opencaching-webserver ./psh.phar docker:init
